3890f4594a9c5061c7de9aa718ccaafcd1285939
Zeynep <creaxx.crea@gmail.com>
2023-05-21 18:11:10 +0300

    PauseMenuAdded

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pausemenu;
+    public string sceneName;
+    public bool toggle;
+    public PlayerController playerScript;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            toggle = !toggle;
+            if (toggle == false)
+            {
+                pausemenu.SetActive(false);
+                Time.timeScale = 1;
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+            if (toggle = true)
+            {
+                pausemenu.SetActive(true);
+                Time.timeScale = 0;
+                playerScript.enabled = false;
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+            }
+        }
+    }
+
+    public void resumeGame()
+    {
+        toggle = false;
+        pausemenu.SetActive(false);
+        Time.timeScale = 1;
+        playerScript.enabled = true;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void QuitTheGame()
+    {
+        Application.Quit();
+    }
+
+
+
+}
